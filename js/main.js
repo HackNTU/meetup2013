f1c692e5a7ce6b2c21d7c695219106453cf1e04d
KD-Chang <ibelieveicaner@gmail.com>
2013-12-13 17:01:56 +0800

    fb like hidden

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@
 	
 // });
 
-window.addEventListener('load', hid(), false);
+window.addEventListener('load', hid, false);
 
 function hid(){
+	alert(1);
 	$('body').css('overflow-x', 'hidden');
 	alert(1);
 }
