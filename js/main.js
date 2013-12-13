8ba67e1d87971ce772cbd7f0a6d30dcd1735d75a
KD-Chang <ibelieveicaner@gmail.com>
2013-12-13 16:47:00 +0800

    fb like hidden

--- js/main.js
+++ js/main.js
@@ -1,22 +1,6 @@
-// $(document).ready(function() {
-// 	var scroller = new FTScroller(document.getElementById('main-content'), {
-// 		scrollingX: false
-// 	});
-//   if(jQuery().onePageNav) {
-//     $('body').onePageNav({
-//       changeHash: false, 
-//       currentClass: 'active', 
-//       filter: 'a[href^="#"]',
-//       scrollOffset: 30,
-//       scrollSpeed: 750, 
-//   	  scrollThreshold: 0.5,
-//   	  easing: 'swing' 
-//     });
-//   }
-//   window.addEventListener('touchstart', function(){
-//   	alert('1');
-//   }, false);
+$(document).ready(function() {
+	document.getElementsByTagName('body')[0].style.cssText = 'oveflow-x: hidden';
 
 
-// });
+});
 
