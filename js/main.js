64728eec657f321e485313f3d336e01e24b31bc0
KD-Chang <ibelieveicaner@gmail.com>
2013-12-13 14:39:50 +0800

    touch

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@ $(document).ready(function() {
 	// var scroller = new FTScroller(document.getElementById('main-content'), {
 	// 	scrollingX: false
 	// });
-  if(jQuery().onePageNav) {
-    $('body').onePageNav({
-      changeHash: false, 
-      currentClass: 'active', 
-      filter: 'a[href^="#"]',
-      scrollOffset: 30,
-      scrollSpeed: 750, 
-  	  scrollThreshold: 0.5,
-  	  easing: 'swing' 
-    });
-  }
+  // if(jQuery().onePageNav) {
+  //   $('body').onePageNav({
+  //     changeHash: false, 
+  //     currentClass: 'active', 
+  //     filter: 'a[href^="#"]',
+  //     scrollOffset: 30,
+  //     scrollSpeed: 750, 
+  // 	  scrollThreshold: 0.5,
+  // 	  easing: 'swing' 
+  //   });
+  // }
   // window.addEventListener('touchstart', function(){
   // 	alert('1');
   // }, false);
